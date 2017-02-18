a22647a3d7f5b00642bafed3f8a540aae0b741ee
Feanturi <bitbucket@mash-coding.de>
2017-02-18 14:05:42 +0100

    Include possibility to write SSML breaks
    as well as more options for outputting an array in SSML

--- src/helper/SSMLHelper.php
+++ src/helper/SSMLHelper.php
@@ -6,6 +6,12 @@
         const BREAK_SOFT = "\\s";
         const BREAK_HARD = PHP_EOL;
 
+        const PAUSE_NONE = 'none';
+        const PAUSE_WEAK = 'weak';
+        const PAUSE_MEDIUM = 'medium';
+        const PAUSE_STRONG = 'strong';
+        const PAUSE_LONG = 'x-strong';
+
         /**
          * parses a $string by \n into a valid SSML-syntaxed message
          *
@@ -42,10 +48,11 @@
          * @param string|null $lastDivider
          * @param string      $title
          * @param string|null $beginSeperator
+         * @param string|null $atEnd
          *
          * @return string
          */
-        public static function sayList (array $list, $divider = null, $lastDivider = null, $title = "", $beginSeperator = null)
+        public static function sayList (array $list, $divider = null, $lastDivider = null, $title = "", $beginSeperator = null, $atEnd = null)
         {
             if (!isset($divider))
                 $divider = self::BREAK_SOFT;
@@ -58,6 +65,12 @@
             if (count($list) > 1)
                 $last = array_pop($list);
 
-            return self::say((($title) ? $title . ':' . $beginSeperator : '') . implode($divider, $list) . (($last) ? $lastDivider . $last : ''));
+            return self::say((($title) ? $title . ':' . $beginSeperator : '') . implode($divider, $list) . (($last) ? $lastDivider . $last : '') . (($atEnd) ? $atEnd : ''));
+        }
+
+        public static function pause ($pauseType = self::PAUSE_STRONG)
+        {
+            $pause = (preg_match('/\\d/', $pauseType)) ? floatval($pauseType) : $pauseType;
+            return '<break ' . ((is_numeric($pause)) ? 'time="' . $pause . 's"' : 'strength="' . $pause . '"') . '/>';
         }
     }
