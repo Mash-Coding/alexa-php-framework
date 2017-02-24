7e642f417e9652be6837855e1a2991e0d07676e7
Marcel Schwettmann <m.schwettmann@neusta.de>
2017-02-24 14:11:56 +0100

    Add documentation for CertHelper methods

--- src/helper/CertHelper.php
+++ src/helper/CertHelper.php
@@ -9,6 +9,14 @@
     {
         const SHA1_BYTES = '3021300906052b0e03021a05000414';
 
+        /**
+         * returns the content of given $certFile (only checking if it exists and whether its empty)
+         *
+         * @param $certFile
+         *
+         * @return null|string
+         * @throws CertificateException
+         */
         public static function getCertificate ($certFile)
         {
             // Download the PEM-encoded X.509 certificate chain that Alexa used to sign the message as specified by the SignatureCertChainUrl header value on the request.
@@ -17,7 +25,7 @@
                 throw new CertificateException("'" . $certFile . "' does not exist");
             else if (!trim($certContent))
                 throw new CertificateException(basename($certFile) . " is empty");
-            
+
             if (DOWNLOAD_CERTS) {
                 $Settings = SettingsHelper::getConfig();
                 FileHelper::writeContentsToFile($Settings->path->cache . FileHelper::getFileName($certFile), $certContent);
@@ -26,12 +34,27 @@
             return $certContent;
         }
 
+        /**
+         * splits given certificate content and splits it into an array of single certificates
+         *
+         * @param $chainOfTrust
+         *
+         * @return array
+         */
         protected static function getCertificatesChainOfTrust ($chainOfTrust)
         {
             preg_match_all('/(?<=-----BEGIN CERTIFICATE-----)(?:\S+|\s(?!-----END CERTIFICATE-----))+(?=\s-----END CERTIFICATE-----)/', $chainOfTrust, $certs);
             return array_map(function ($cert) { return "-----BEGIN CERTIFICATE-----\n" . trim($cert) . "\n-----END CERTIFICATE-----"; }, $certs[0]);
         }
 
+        /**
+         * this method tries to retrieve the parent certificate by given X.509 certificate.
+         * null is returned when no parent certificate is found.
+         *
+         * @param X509 $X509
+         *
+         * @return string|null
+         */
         protected static function getImmediateCertificate (X509 $X509)
         {
             $extension = $X509->getExtension('id-pe-authorityInfoAccess');
@@ -44,7 +67,14 @@
             return null;
         }
 
-        protected static function getSubjectAlternativeNames (X509 $X509, $nameOnly = false)
+        /**
+         * gets an array containing all alternative domains of subject in given X.509 certificate
+         *
+         * @param X509 $X509
+         *
+         * @return array
+         */
+        protected static function getSubjectAlternativeNames (X509 $X509)
         {
             $names = [];
             $extension = $X509->getExtension('id-ce-subjectAltName');
@@ -58,6 +88,14 @@
             return $names;
         }
 
+        /**
+         * compares the signatures of the two given certificate strings
+         *
+         * @param $cert1Content
+         * @param $cert2Content
+         *
+         * @return bool
+         */
         protected static function compareCertificateSignatures ($cert1Content, $cert2Content)
         {
             $X509 = new X509();
@@ -66,6 +104,21 @@
             return $cert1['signature'] === $cert2['signature'];
         }
 
+        /**
+         * verifies a given certificate string by splitting it into single certificates (if its a chained certificate)
+         * and then iterates through all certificates and validates for each of it:
+         *  - has the certificate expired?
+         *  - does one of certificates subject alternative names match the acceptedSignature URLs from the config?
+         *  - is it the last certificate of the chain and thus has no parent certificate?
+         *  - if it's not the last one and it has a parent certificate
+         *  - does the next in line certificate match the defined parent certificate (compared by signature)?
+         *  - is the certificate valid in general?
+         *
+         * @param $certContent
+         *
+         * @return array containing the validated X509 objects
+         * @throws CertificateException
+         */
         public static function verifyCertificate ($certContent)
         {
             $Settings = SettingsHelper::getConfig();
@@ -119,12 +172,24 @@
             return $certs;
         }
 
+        /**
+         * @return JSONObject
+         */
         public static function getLoadedCertificates ()
         {
             return DataHandler::getDataObject()->certs;
         }
 
-        public static function checkCertificate ($certFile, $signature = null)
+        /**
+         * initialized the verification process for given $certFile
+         *
+         * @param      $certFile
+         *
+         * @return array
+         * @throws CertificateException
+         * @see CertHelper::verifyCertificate()
+         */
+        public static function checkCertificate ($certFile)
         {
             $Certificates = self::getLoadedCertificates();
             $Certificates->push(basename($certFile));
